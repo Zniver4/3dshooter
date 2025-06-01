2631a7453691960574a3b0d1a6f547694d29a56d
KurenaiOuji <126101022+KurenaiOuji@users.noreply.github.com>
2025-06-01 17:00:33 -0600

    Disparo
    Se puede Disparar
    Tambien Implemente mi movimiento

---
+++ 3dshooter/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform PlayerCamera;
+    private CharacterController _controller;
+
+    [Space(5)]
+    [Header("Movement Settings")]
+    [SerializeField] private float Speed = 5f;
+    [SerializeField] private float Gravity = 9.81f;
+
+    private float _verticalVelocity;
+
+    [Space(5)]
+    [Header("Camera Settings")]
+    [Tooltip("Camera movement speed")]
+    [SerializeField] private Vector2 Sensitivity;
+    [Tooltip("Max range the camera can rotate")]
+    [SerializeField] private float Degree = 90f;
+    private Vector2 XYRotation;
+
+    [Space(5)]
+    [Header("Inputs")]
+    private float HorizontalInput;
+    private float VerticalInput;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        InputManagement();
+        Movement();
+    }
+
+    private void Movement()
+    {
+        GroundMovement();
+        CameraRotation();
+    }
+
+    private void GroundMovement()
+    {
+        Vector3 move = new Vector3 (HorizontalInput, 0, VerticalInput);
+        move = transform.TransformDirection(move);
+
+        move *= Speed;
+
+        move.y = ApplyGravity();
+
+        _controller.Move(move * Time.deltaTime);
+    }
+
+    private float ApplyGravity()
+    {
+        if (_controller.isGrounded)
+        {
+            _verticalVelocity = -1f;
+        }
+        else
+        {
+            _verticalVelocity -= Gravity * Time.deltaTime;
+        }
+
+        return _verticalVelocity;
+    }
+
+    private void InputManagement()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void CameraRotation()
+    {
+        Vector2 _mouseInput = new Vector2
+        {
+            x = Input.GetAxis("Mouse X"),
+            y = Input.GetAxis("Mouse Y")
+        };
+
+        XYRotation.x -= _mouseInput.y * Sensitivity.y;
+        XYRotation.y += _mouseInput.x * Sensitivity.x;
+
+        XYRotation.x = Mathf.Clamp(XYRotation.x, -Degree, Degree);
+
+        transform.eulerAngles = new Vector3(0f, XYRotation.y, 0f);
+        PlayerCamera.localEulerAngles = new Vector3(XYRotation.x, 0f, 0f);
+    }
+}
---
+++ 3dshooter/Assets/Scripts/Player/WeaponController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    [Header("References")]
+    [Tooltip("Lugar del cual saldra el RayCast del disparo")]
+    [SerializeField] private Transform playerCamera;
+
+    [Header("RayCast")]
+    [Tooltip("Rango Maximo del Disparo")]
+    [SerializeField] private float range = 15f;
+
+    [Header("Damage")]
+    [Tooltip("Daño del Disparo")]
+    [SerializeField] private int damage = 25;
+
+    [Header("LayerMask")]
+    [Tooltip("Objetos a los que le Puedes Hacer Daño")]
+    [SerializeField] private LayerMask layerMask;
+
+    private void Update()
+    {
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 rayOrigin = playerCamera.transform.position;
+            Vector3 rayDirection = playerCamera.transform.forward;
+
+            RaycastHit hitInfo;
+
+            if(Physics.Raycast(rayOrigin, rayDirection, out hitInfo, range, layerMask))
+            {
+                Debug.DrawRay(rayOrigin, rayDirection * hitInfo.distance, Color.green, 1f);
+
+                IDamageable damageable = hitInfo.collider.GetComponent<IDamageable>();
+
+                if(damageable != null)
+                {
+                    damageable.ApplyDamage(damage);
+                }
+
+                else
+                {
+                    Debug.DrawLine(rayOrigin, rayDirection * range, Color.red, 1f);
+                }
+            }
+        }
+    }
+}
